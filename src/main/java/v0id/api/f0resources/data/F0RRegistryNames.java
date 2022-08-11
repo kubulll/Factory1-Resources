0a4be620e5791ff52003a72b96c2155393fbae21
Gliese-832-c <gliesec832@gmail.com>
2022-08-11 21:19:37 +0200

    - Updated the version number to 2.1.0 as 2.0.0 was already uploaded onto CurseForge - Gave the Seismic Reader a crafting recipe

--- src/main/java/v0id/api/f0resources/data/F0RRegistryNames.java
+++ src/main/java/v0id/api/f0resources/data/F0RRegistryNames.java
@@ -9,7 +9,7 @@ public class F0RRegistryNames
 {
     public static final String MODID                                                            = "f0-resources";
     public static final String MOD_NAME = "Factory1 - Resources";
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "2.1.0";
     public static final String MCVERSION = "1.12.2";
 
     public static final String
